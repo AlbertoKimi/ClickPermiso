26bd619846a06c114fa45757d14c33543b6d69b2
AlbertoKimi <breakingdawn1988@gmail.com>
2026-02-10 20:51:27 +0100

    Formulario mi perfil modificado

--- Click-Permiso/src/pages/mis-dias.tsx
+++ Click-Permiso/src/pages/mis-dias.tsx
@@ -1,8 +1,7 @@
-import { MiPerfil_form } from './components/MiPerfil_form.tsx';
+
 
 export const Solicitados = () =>(
     <>
         <h2>solicitado</h2>
-        <MiPerfil_form></MiPerfil_form>
     </>
 )
