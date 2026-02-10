1b840a23efde74ec58a0e7777a76aa23fcd77408
AlbertoKimi <breakingdawn1988@gmail.com>
2026-02-10 18:22:35 +0100

    Formulario nocturno hecho.

--- Click-Permiso/src/pages/nocturno.tsx
+++ Click-Permiso/src/pages/nocturno.tsx
@@ -1,5 +1,10 @@
+import { Formulario } from "../componentes/Formulario";
+
 export const Nocturno = () =>(
-    <>
-        <h2>NOCTURNO</h2>
-    </>
+    <div className="flex flex-col items-center justify-center min-h-[50vh] gap-10">
+        <h2 className="text-3xl font-bold text-indigo-400 mb-6 tracking-widest uppercase border-b-2 border-indigo-500/50 pb-2">
+            NOCTURNO
+        </h2>
+        <Formulario></Formulario>
+    </div>
 )
